3c9d0728c130f133b13f8f17441a813780ccc350
ishino <ishino.y@opst.co.jp>
2016-05-23 15:30:01 +0900

    add practice

--- ScalaPractice/src/jp/co/opst/hello_scala/Practice01_IO.scala
+++ ScalaPractice/src/jp/co/opst/hello_scala/Practice01_IO.scala
@@ -4,9 +4,10 @@ import scala.util.control.Breaks.{break,breakable}
 
 
 /**
- * 標準入力とループ途中でのbreak練習
+ * @author Yui
+ * 標準入力とループ途中でのbreak練習クラス
  */
-object ScalaPractice01 {
+object ScalaPractice01_IO {
 
   def main(args: Array[String]) = {
 
@@ -19,6 +20,9 @@ object ScalaPractice01 {
     val lines = io.Source.stdin.getLines();
     var counter: Int = 0;
 
+    // breakable ブロックでかこみ、
+    // その中でbreakすると
+    // breakable から抜け出せる
     breakable {
 
       for (line <- lines) {
@@ -31,6 +35,18 @@ object ScalaPractice01 {
 
     }
     println("------------------------------------")
+
+    println("java.util.Scannerを使用した入力")
+    println("単一行入力↓")
+    val scanner = new java.util.Scanner(System.in)
+
+    // 5回ループ
+    for(i <- 0 to 4) {
+      println(scanner.next())
+    }
+    scanner.close()
+
+
 
   }
 }
---
+++ ScalaPractice/src/jp/co/opst/hello_scala/Practice02_match.scala
@@ -0,0 +1,44 @@
+package jp.co.opst.hello_scala
+
+/**
+ * @author Yui
+ * Scalaのmatch構文(Javaでいうswitch)の練習クラス
+ */
+object Practice02_match {
+  def main(args: Array[String]) = {
+
+    println("アルファベット検査")
+    print("a～cのアルファベットを入力してください -->")
+    val input = io.StdIn.readLine()
+
+    input match {
+
+      // breakは不要
+      case "a" => print("a")
+      case "b" => print("b")
+      case "c" => print("c")
+
+      // Javaのdefaultに相当
+      case _ => print("不正な文字列")
+    }
+
+    println("が入力されました")
+
+    println("------------------------------")
+
+    println("データ型検査")
+    print("整数か文字列を入力してください")
+
+    val input2:Any = io.StdIn.readLine()
+
+    input2 match {
+      case i:Int => println(i + "は数字です")
+      case s:String => println(s + "は文字列です")
+      case d:Double => println(d + "は小数です")
+      case _ => (input2 + "は数字でも文字列でもないものです")
+    }
+
+
+  }
+
+}
